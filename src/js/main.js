b0df01d6b59fe12ee7f37734bc8c8feee205c703
Maria Luisa Lufrano <marialuisa.lufrano@icloud.com>
2022-07-16 00:12:04 +0200

    add scheme js and gitignore

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/*  PUTNO 1
+    1.1. Realizar estructura basica html (h1, input te tipo texto, boton "buscar" y un boton "reset", listado de resultados donde tenemos el carte de la serie osea foto y el titulo)
+
+    PUNTO 2
+    1. Escuchar un evento sobre el boton de busqueda para conectarse al API para ver que datos necesitamos
+    2. Para la URL hay que recoger el texto del input de la usuaria y concatenarlo en link
+    3. Para cada serie del recultado del input hay que pintar el cartel y el titulo de la serie
+    4. Algunas series no tiene foto, hay que detectar cuales son y asignarle una foto (https://via.placeholder.com/210x295/ﬀﬀﬀ/666666/?text=TV)
+    5.Para pintar en el html utilizamos el InnerHTML
+
+    PUNTO 3
+*/
