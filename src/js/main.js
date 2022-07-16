c7fca34dbf276757380601a58dd4768559d93fb5
Maria Luisa Lufrano <marialuisa.lufrano@icloud.com>
2022-07-16 18:28:42 +0200

    Add structure and scheme for the exercise

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,28 @@
 'use strict';
 
-/*  PUTNO 1
-    1.1. Realizar estructura basica html (h1, input te tipo texto, boton "buscar" y un boton "reset", listado de resultados donde tenemos el carte de la serie osea foto y el titulo)
+/* PUNTO 1
+1. Realizar estructura basica html (h1, input te tipo texto, boton "buscar" y un boton "reset", listado de resultados donde tenemos el cartel de la serie osea foto y el titulo)
 
-    PUNTO 2
-    1. Escuchar un evento sobre el boton de busqueda para conectarse al API para ver que datos necesitamos
-    2. Para la URL hay que recoger el texto del input de la usuaria y concatenarlo en link
-    3. Para cada serie del recultado del input hay que pintar el cartel y el titulo de la serie
-    4. Algunas series no tiene foto, hay que detectar cuales son y asignarle una foto (https://via.placeholder.com/210x295/ﬀﬀﬀ/666666/?text=TV)
-    5.Para pintar en el html utilizamos el InnerHTML
+PUNTO 2
+1. Escuchar un evento sobre el boton de busqueda para conectarse al API para ver que datos necesitamos
+2. Para la URL hay que recoger el texto del input de la usuaria y concatenarlo en link
+3. Para cada serie del recultado del input hay que pintar el cartel y el titulo de la serie
+4. Algunas series no tiene foto, hay que detectar cuales son y asignarle una foto (https://via.placeholder.com/210x295/ﬀﬀﬀ/666666/?text=TV)
+5.Para pintar en el html utilizamos el InnerHTML
 
-    PUNTO 3
+PUNTO 3
+1. Una vez que se vean los resultados de la busqueda, la usuario puede elegir los favoritos y hay que hacer click en una serie y tendra que pasar lo siguiente:
+--> color de fondo y de fuente se intercambian
+--> listado de favoritos en la izquierda (recomendable array para almacenar favoritos)
+--> eries favoritas deben seguir apareciendo a la izquierda aunque la usuaria realice otra búsqueda
+
+PUNTO 4
+1.Hay que almacenar el listado de favoritos en el localStorage. De esta forma, al recargar la página el listado de favoritos se debe mostrarse
+
+PUNTO 5 BONUS
+1. Borrar favoritos del localStorage y otras cosas mas. Mirar el enunciado
+
+PUNTO 6
+1. Afinar la maquetacion
 */
+
